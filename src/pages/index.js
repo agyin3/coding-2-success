f5c35193f17d26f3110b0c252b774e1820345018
Buddy Agyin <agyin3@gmail.com>
2020-05-17 14:52:19 -0600

    fix typo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Hero from "../components/Hero/Hero"
 import About from "../components/About/About"
 import Signup from "../components/Signup/Signup"
-import SEO from "../utils/seo"
+import SEO from "../utils/SEO"
 
 export default () => {
     return (
